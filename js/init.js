293e04dacde6a75c4cf693eec11507b29f561b15
Hélcio Lopes <helciokarman@gmail.com>
2020-01-22 22:05:59 -0300

    Colocando para salvar as mensagens no firebase e nao sobrescrever

---
+++ js/init.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyARQZatfj796Q88Yt134jepGVbO4DDhpJQ",
+    authDomain: "linkme-cd21d.firebaseapp.com",
+    databaseURL: "https://linkme-cd21d.firebaseio.com",
+    projectId: "linkme-cd21d",
+    storageBucket: "linkme-cd21d.appspot.com",
+    messagingSenderId: "74019781068",
+    appId: "1:74019781068:web:0c223238eae70b8f118569"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
---
+++ js/linkme.js
@@ -0,0 +1,11 @@
+function enviar() {
+    var emailValue = document.getElementById('emailTxt').value;
+    var areaValue = document.getElementById('areaTxt').value;
+    var timestamp = new Date().getTime();
+    // A variável database vai receber as funções de acesso ao banco de dados
+    var database = firebase.database();
+    database.ref(timestamp).set({
+        email: emailValue,
+        mensagem: areaValue,
+    });
+}
